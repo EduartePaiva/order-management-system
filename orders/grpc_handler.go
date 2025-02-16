5f2dc8c381482543ebd65ac8197be6e67959b0d8
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-02-16 15:49:48 -0300

    validating errors

---
+++ orders/grpc_handler.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/eduartepaiva/order-management-system/common/api"
+	"google.golang.org/grpc"
+)
+
+type grpcHandler struct {
+	pb.UnimplementedOrderServiceServer
+}
+
+func NewGRPCHandler(grpcServer *grpc.Server) {
+	handler := &grpcHandler{}
+	pb.RegisterOrderServiceServer(grpcServer, handler)
+}
+
+func (h *grpcHandler) CreateOrder(ctx context.Context, p *pb.CreateOrderRequest) (*pb.Order, error) {
+	log.Printf("New order received! Order %v", p)
+
+	return &pb.Order{ID: "42"}, nil
+}
