f18a73f08ccda40727c8b3963e479e8e8673b07d
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-02-16 14:53:29 -0300

    Handler building

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -1,17 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/eduartepaiva/order-management-system/common"
+	pb "github.com/eduartepaiva/order-management-system/common/api"
+)
 
 type handler struct {
 	//gateway
+	client pb.OrderServiceClient
 }
 
-func NewHandler() *handler {
-	return &handler{}
+func NewHandler(client pb.OrderServiceClient) *handler {
+	return &handler{client}
 }
 
 func (h *handler) registerRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("POST /api/customers/{customerID}/orders", h.handleCreateOrder)
 }
 
-func (h *handler) handleCreateOrder(w http.ResponseWriter, r *http.Request) {}
+func (h *handler) handleCreateOrder(w http.ResponseWriter, r *http.Request) {
+	customerID := r.PathValue("customerID")
+
+	var items []*pb.ItemsWithQuantity
+	if err := common.ReadJSON(r, &items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+		CustomerID: customerID,
+		Items:      items,
+	})
+}
--- gateway/main.go
+++ gateway/main.go
@@ -5,17 +5,31 @@ import (
 	"net/http"
 
 	_ "github.com/joho/godotenv/autoload"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/eduartepaiva/order-management-system/common"
+	pb "github.com/eduartepaiva/order-management-system/common/api"
 )
 
 var (
-	httpAddr = common.EnvString("HTTP_ADDR", ":8080")
+	httpAddr         = common.EnvString("HTTP_ADDR", ":8080")
+	orderServiceAddr = "localhost:3000"
 )
 
 func main() {
+	conn, err := grpc.NewClient(orderServiceAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to dial server: %v", err)
+	}
+	defer conn.Close()
+
+	log.Println("Dialing orders service at ", orderServiceAddr)
+
+	c := pb.NewOrderServiceClient(conn)
+
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(c)
 	handler.registerRoutes(mux)
 
 	log.Printf("Starting http server at port %s", httpAddr)
